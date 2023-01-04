3081282dbafc78261191d4e0992d5441cb0f7c99
ErionaM <121973293+ErionaM@users.noreply.github.com>
2023-01-04 20:27:57 +0100

    Add files via upload

---
+++ myScipt.js
@@ -0,0 +1,13 @@
+//qdo qe te munum me shkru brenda tagut 
+//tagu alert nuk e vazhdon deri te shtypum vazhdo 
+function revers(){
+    alert:'test';
+document.getElementById('text').innerHTML='paragrafi 0';
+}
+function change() {
+    document.getElementById('text').innerHTML = 'Hello javascript';
+}
+function font() {
+    document.getElementById('text').style.fontSize='30px';
+}
+//
